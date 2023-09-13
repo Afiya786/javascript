a806f4c5b5b280097dd87ed29fc02ce5a32d6f0d
Afiya786 <75537823+Afiya786@users.noreply.github.com>
2023-09-13 20:32:29 +0530

    dataypes

---
+++ datatypes.js
@@ -0,0 +1,8 @@
+//when you write "use strict"; in the top your js code will be treated as newer version of js code
+
+// 1. number
+// 2. bigint
+// 3. boolean
+// 4.string
+// 5.undefined
+// 6. null
