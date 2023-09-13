7568feada60f7b202e7d2377a1b0495dbc34f7c6
Afiya786 <75537823+Afiya786@users.noreply.github.com>
2023-09-13 22:58:26 +0530

    new conversion file added

---
+++ stringnumconvert.js
@@ -0,0 +1,7 @@
+console.log("1" + 2) // 12
+console.log(1 + "2") // 12
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 + "2") // 32
+
+// note: if our string is  at first place than all the other numbers are treated as string
+// and if the first is number than every other is treated as number
