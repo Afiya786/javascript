2b2c726d45e39aa00e9193cbfa884b12ad31aecd
Afiya786 <75537823+Afiya786@users.noreply.github.com>
2023-09-13 20:31:10 +0530

    we are heading towards js slowly

--- var.js
+++ var.js
@@ -2,3 +2,7 @@ const id = 12345 //you cannot change const value
 let constname ="afiya" //only use let and const
 var password = "afiyamalik" //var is having problem of scope as the var with same name gets changes even if you are changing the different var
 console.table([id,constname,password]);
+ 
+
+//if you make a variable and put it without anyvalue than its value will be undefined on running
+ex: let name;//
