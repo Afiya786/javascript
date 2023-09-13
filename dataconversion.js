721fb7021c1a966909b43539a59098cb97da50f1
Afiya786 <75537823+Afiya786@users.noreply.github.com>
2023-09-13 22:41:08 +0530

    conversion file added

---
+++ dataconversion.js
@@ -0,0 +1,7 @@
+let score = "33"
+let num = Number(score)
+console.log(typeof(num))
+
+console.log(Boolean(1))//true
+console.log(Boolean(0))// false
+console.log(Boolean("afiya")) //string will also return true
