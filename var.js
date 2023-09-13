2167482d67333445baf1dcb95b631bf9e891a92d
Afiya786 <75537823+Afiya786@users.noreply.github.com>
2023-09-13 20:18:59 +0530

    comments added

--- var.js
+++ var.js
@@ -1,4 +1,4 @@
 const id = 12345 //you cannot change const value
-let constname ="afiya"
+let constname ="afiya" //only use let and const
 var password = "afiyamalik" //var is having problem of scope as the var with same name gets changes even if you are changing the different var
 console.table([id,constname,password]);
