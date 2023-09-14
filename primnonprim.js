d0b9e7d29604f11684546eb3bf820ffafdcc5e8a
Afiya786 <75537823+Afiya786@users.noreply.github.com>
2023-09-14 11:02:44 +0530

    primitive and non-primitive data types

---
+++ primnonprim.js
@@ -0,0 +1,12 @@
+// Primitive data types are the fundamental data types built into a programming language.
+// They are used to represent single values.
+// They are usually immutable, meaning their values cannot be changed once they are assigned.
+// Examples of primitive data types in JavaScript include numbers, strings, booleans, null, undefined, symbols (ES6), and bigints (ES11).
+
+
+//---------------------------><--------------//
+// Non-Primitive Data Types (Complex Data Types or Reference Data Types):
+// Non-primitive data types are more complex data structures that can hold multiple values or objects.
+
+
+// Examples of non-primitive data types in JavaScript include objects, arrays, functions, and custom classes or instances created using constructors.
