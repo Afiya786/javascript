ca3d100e5fe124d40df1dc159c03c74ac6ede727
Afiya786 <75537823+Afiya786@users.noreply.github.com>
2023-09-17 08:15:06 +0530

    array concepts

--- Arrayintro.js
+++ Arrayintro.js
@@ -1,5 +1,28 @@
 // #how you store and manage data
 
 // javascript array copy operation create shallow copy not deep copy
-// shallow copy : of an object is a copy whose properties share the same Reference (point to the same underlying values)
-// as those of the source object 
+// shallow copy : of an object is a copy whose properties share the same Reference point to the same underlying values  as those of the source object original gets change directly
+
+
+// const array = [0,1,2,3,4,5]
+// const myHero = ["shaktiman", "batman"]
+
+const array2 = new Array(1,2,3,4,5,6)
+// array2.pop()
+// console.log(array2.pop())
+
+//slice , splice difference
+
+const myn = array2.slice(1,3) //include 1 exclude 3 return [1,2]index
+console.log(myn)
+console.log(array2)
+
+const myn2 = array2.splice(1,3)
+console.log(myn2)
+ console.log(array2) 
+
+//slice does not change the original array it just gives the value accordingly but splice manipulates the original array
+
+
+
+
