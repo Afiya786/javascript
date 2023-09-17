63dcbcc4434a249ff7804bda3008de6d560bf199
Afiya786 <75537823+Afiya786@users.noreply.github.com>
2023-09-17 07:15:06 +0530

    modification

---
+++ Arrayintro.js
@@ -0,0 +1,5 @@
+// #how you store and manage data
+
+// javascript array copy operation create shallow copy not deep copy
+// shallow copy : of an object is a copy whose properties share the same Reference (point to the same underlying values)
+// as those of the source object 
