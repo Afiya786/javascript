86f576ded82b94b451c2f8039cceaa73e5128819
Afiya786 <75537823+Afiya786@users.noreply.github.com>
2023-09-18 07:26:29 +0530

    objets par2

--- objects.js
+++ objects.js
@@ -1,31 +1,39 @@
-// const hi = {} //empty object
+const new1 ={}
+new1.name = "afiya"
+new1.age =21
+new1.address = "colony"
+console.log(new1)
 
-// const newobj = {
-//     name: "afiya",
-//     email:"afiya100@gmail.com",
-//     phone: 452455,
-//     isLogin: false
-// }
-// console.log(newobj.name)
-// console.log(newobj["phone"])
+const new2 = {
+    name : "hammad",
+    age : 24,
 
-// // question : take one symbol and add them to object and print them 
+    new3: {
+        password:"hello",
+        city: "delhi"
 
-// const mysymbol = Symbol("key1")
-
-// const user = {
-//      [mysymbol] : "key1" //this is the syntax for symbol when using under object
-// }
-// console.log(mysymbol)
-// console.log(user[mysymbol])
-
-// const mysym = Symbol("afiya")
-// console.log(typeof(mysym)) //normal symbol
-
-// console.log(user)
-
-const hello = {
-    name: "afiya",
-    age: 22
+    }
 }
-console.log(hello.name)
+
+console.log(Object.keys(new2)) // the output must be in array can be used many in many ways 
+//and vice-versa with values ; Object.values(new2)
+
+//one more : entries console.log(Object.entries(new2))
+//this will give each value as array i.e array insie array
+
+
+console.log(new2.name , new1.name)
+
+// ----combining two or more objects---//
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"c", 4:"d"}
+// //you cant choose concat method
+// const obj3 = Object.assign({},obj1,obj2)//here empty bracket serve as a target and obj1 and obj2 are served as source
+// console.log(obj3)
+
+
+// //second method i.e spread method
+
+const obj4 = {...obj1,...obj2}
+console.log(obj4);
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,5 +1,2 @@
-console.log(mysymbol)
-console.log(user[mysymbol])
-
-const mysym = Symbol("afiya")
-console.log(typeof(mysym)) //normal symbol
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"c", 4:"d"}
