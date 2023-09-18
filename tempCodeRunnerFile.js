51b1c55ea10fa6391bdd44944a73d5cae8f22a6e
Afiya786 <75537823+Afiya786@users.noreply.github.com>
2023-09-18 20:50:33 +0530

    scope level

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,6 +1,11 @@
-let a = 10
-function scope(){
-    let a = 20
-    const b =30
-    console.log("this is local", a);
+function one(){
+    const username ="afiya"
+
+    function two(){
+        const website ="youtube"
+        console.log(username)
+
+    }
+    two()
 }
+one()
