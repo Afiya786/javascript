2a4ce15b2ada6a6d07f35e1de9b2fb1b1ae3fc70
Afiya786 <75537823+Afiya786@users.noreply.github.com>
2023-09-23 16:09:02 +0530

    await keyword

---
+++ await.js
@@ -0,0 +1,22 @@
+const fetchuserData = new Promise(function(resolve,reject){
+  setTimeout(() =>{
+    const data = {username :"maktub", password:"afiya"};
+    resolve(data);
+  }, 2000);
+});
+
+// Now, we can use the async/await syntax to work with promises more easily.
+
+async function getUserData() {
+    try {
+        console.log("fetching user data");
+        const user = await fetchuserData;
+        console.log("user data", user);
+    } catch (error) {
+        console.error("Error", error);
+    }
+
+}
+
+getUserData();
+console.log("request in progress");
